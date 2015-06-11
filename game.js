34033a8956f1c216e0b801f1dda7381a28039449
raingxm <raingxm@163.com>
2015-06-11 22:12:00 +0800

    too many bullets from invaders, reduce bullets number

--- game.js
+++ game.js
@@ -107,7 +107,7 @@
             this.center.x += this.speedX;
             this.patrolX += this.speedX;
 
-            if(Math.random() > 0.95 ) {
+            if(Math.random() > 0.995 ) {
                 var bullet = new Bullet({x: this.center.x, y: this.center.y + this.size.x },
                     {x: Math.random() - 0.5, y: 2});
                 this.game.addBody(bullet);
